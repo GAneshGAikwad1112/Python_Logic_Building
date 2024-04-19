44f1648ce5b4f3da188cc8c61f303285b52f4860
ganeshgaikwad1112 <gaikwadg1112@gmail.com>
2024-04-19 10:18:10 +0530

    adding some more files

---
+++ python1.py
@@ -0,0 +1,11 @@
+# write a python program which will keep adding a stream of numbers inputted by the user. The adding stops as soon  user presses q key on the keyboard.
+
+sum = 0
+while(True):
+    userInput = input("Enter the price ir press q to quit:  \n")
+    if (userInput != 'q'):
+        sum = sum + int(userInput)
+        print(f"Ordet total so far: {sum}")
+    else:
+        print(f"Your Bill total is {sum}. Thanks for shopping with us")
+        break
---
+++ python2.py
@@ -0,0 +1,3 @@
+# How to find Factorial and no of Trailing zeros in a factorial of a number in python
+
+
